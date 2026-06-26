b2957f44e2393b2e17459000a3f67161be1ddfb2
velasquezpazanamaria8-oss <velasquezpazanamaria8@gmail.com>
2026-06-26 18:54:19 -0500

    asdasdasasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasd

--- login.php
+++ login.php
@@ -38,7 +38,11 @@ if($_SERVER['REQUEST_METHOD']==='POST'){
       <div class="login-logo-icon">
         <svg fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"/></svg>
       </div>
-      <h1>Conta<span>Docs</span></h1>
+      <!-- <h1>Conta<span>Docs</span></h1> -->
+       <a href="/index.php" class="btn-cta-login">
+        Conta<span>Docs</span>
+      </a>
+
       <p>Portal de documentos contables</p>
     </div>
     <div class="login-card">
