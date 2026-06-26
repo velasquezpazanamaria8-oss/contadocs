887341cd87ad212b20b02f6f546e7f37fedff00a
velasquezpazanamaria8-oss <velasquezpazanamaria8@gmail.com>
2026-06-26 01:22:07 -0500

    asdsadasasd

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   typescript: {
-    // En producción Prisma Client se genera antes del build
-    // ignoreBuildErrors solo para desarrollo sin BD conectada
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
 }
 
